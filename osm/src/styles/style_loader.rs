30cfda6e1341d2176206f1e59137e80317ffdc2b
Kirill Olenev <olenyov.kirill@me.com>
2026-07-16 21:17:29 +0900

    Support loading from local file

--- osm/src/styles/style_loader.rs
+++ osm/src/styles/style_loader.rs
@@ -1,6 +1,7 @@
 use crate::source::reqwest_source::ReqwestSource;
 use crate::styles::Style;
 use log::error;
+use std::fs;
 use thiserror::Error;
 
 pub struct StyleLoader;
@@ -12,7 +13,11 @@ pub enum StylesFetchError {
 }
 
 impl StyleLoader {
-    pub fn load() -> Vec<Style> {
+    pub fn load(local_file: bool) -> Vec<Style> {
+        if local_file {
+            return serde_json::from_slice(fs::read("styles_v0.json").as_ref().unwrap()).unwrap();
+        }
+
         let styles = ReqwestSource::new().styles();
         if let Err(err) = styles.as_ref() {
             error!("Error loading styles: {:?}", err);
